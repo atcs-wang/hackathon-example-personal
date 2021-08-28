60b94f044038e5705ab7060b85de653eb7dda495
Matthew Wang <matwan@bergen.org>
2021-08-28 01:39:50 -0400

    Simple mysql database model (event, attendees, event_interest w/ triggers)

---
+++ db/event_interest_before_delete_trigger.sql
@@ -0,0 +1,4 @@
+CREATE DEFINER = CURRENT_USER TRIGGER `hackbca_example`.`event_interest_BEFORE_DELETE` BEFORE DELETE ON `event_interest` FOR EACH ROW
+BEGIN
+	UPDATE events SET event_interest = event_interest - 1 WHERE event_id = OLD.event_id;
+END
