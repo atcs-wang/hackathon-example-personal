92e01158f2c913da1ead95e5625d34f858229e8d
Matthew Wang <matwan@bergen.org>
2021-08-31 22:41:13 -0400

    Index hbs/route and layout hbs

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("here!");
-  res.render('index', { title: 'Express', layout:"layout"}); //layout:"layout" is the default, but you can choose an alternative layout
+  res.render('index', { title: 'HackBCA 20XX', 
+                        layout:"layout", 
+                        style: "index"
+                      }); //layout:"layout" is the default, but you can choose an alternative layout
   //https://stackoverflow.com/questions/26871522/how-to-change-default-layout-in-express-using-handlebars
 });
 
