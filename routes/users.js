a4a9f12cc60142b8f5521fa4576ea9b204465f21
Matthew Wang <matwan@bergen.org>
2021-11-18 14:38:59 -0500

    prototype and views updates

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('loginform', {
+    register: false
+  });
+});
+
+router.post('/login', function(req, res, next) {
+  console.log("LOGIN:" + JSON.stringify(req.body));
+  res.redirect("/");
+});
+
+
+router.get('/register', function(req, res, next) {
+  res.render('loginform', {
+    register: true
+  });
+});
+
+router.post('/register', function(req, res, next) {
+  console.log("REGISTER:" + JSON.stringify(req.body));
+  res.redirect("/");
+});
+
 module.exports = router;
